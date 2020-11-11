39fd2f30f81f3d55dfa26a6beb4de58017bd7df6
Eduardo Martinez Alcaraz <e_m_a_rrr@hotmail.com>
2020-11-11 17:17:49 -0600

    cambio a sw

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 ///cache no modificable
 const INMUTABLE_CACHE = 'inmutable-v1';
