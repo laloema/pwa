71bdcee3682031a278b05a663c0f1548c0f44198
Eduardo Martinez Alcaraz <e_m_a_rrr@hotmail.com>
2020-11-12 16:14:39 -0600

    modificado sw

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v3';
+const DYNAMIC_CACHE = 'dynamic-v2';
 ///cache no modificable
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -54,7 +54,9 @@ self.addEventListener('activate',e=>{
             if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
-
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
         });
 
     });
